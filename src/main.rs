e96ce3c449972be7854545bf0c5d9c386eccbd42
Niclas <niclas@nidomiro.de>
2022-02-09 10:28:56 +0100

    feat: addition is working; ignoring IntelliJ folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a calculation");
+
+    let mut input = String::new();
+
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line");
+
+    let parts: Vec<i32> = input.split(['+'])
+        .map(|x| x.trim().parse().expect("Only numbers are allowed"))
+        .collect();
+
+    if parts.len() != 2 {
+        panic!("Currently only + is a supported operator")
+    }
+
+
+    println!("Parts: {:?}", &parts);
+
+    println!("{} + {} = {}", parts[0], parts[1], parts.iter().sum::<i32>())
 }
