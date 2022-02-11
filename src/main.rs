0df617196b62227d7d33e3bf614714c68b0a4ad6
Niclas <niclas@nidomiro.de>
2022-02-11 20:43:45 +0100

    feat: moves from struct to enum to increase expressiveness

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,78 @@
-use regex::{Regex};
+use regex::{Captures, Regex};
 
 #[derive(Debug)]
-struct InvalidOperatorError { operator: char }
+enum MathStringParseError {
+    NotParsable,
+    InvalidOperatorError {
+        operator: char,
+    }
+}
 
 #[derive(Debug)]
-struct Operation {
-    left: i32,
-    operator: char,
-    right: i32,
+enum Operation {
+    Number(f64),
+    Addition {
+        left_addend: Box<Operation>,
+        right_addend: Box<Operation>,
+    },
+    Subtraction {
+        minuend: Box<Operation>,
+        subtrahend: Box<Operation>,
+    },
+    Multiplication {
+        multiplicand: Box<Operation>,
+        multiplier: Box<Operation>,
+    },
+    Division {
+        dividend: Box<Operation>,
+        divisor: Box<Operation>,
+    },
 }
 
 impl Operation {
-    fn from_string(input: &String) -> Option<Operation> {
-        let regex = Regex::new(r"^\s*(\d+)\s*([+\-*/])\s*(\d+)\s*$").unwrap();
+    fn from_string(input: &String) -> Result<Operation, MathStringParseError> {
+        let regex = Regex::new(r"^\s*(\d+)\s*(\S)\s*(\d+)\s*$").unwrap();
 
-        regex.captures(&input).map(|capture| Operation {
-            left: (&capture[1]).parse().unwrap(),
-            operator: (&capture[2]).parse().unwrap(),
-            right: (&capture[3]).parse().unwrap(),
-        })
+        fn from_capture(capture: Captures) -> Result<Operation, MathStringParseError> {
+            let operator: char = (&capture[2]).parse().unwrap();
+            let left_side: f64 = (&capture[1]).parse().unwrap();
+            let right_side: f64 = (&capture[3]).parse().unwrap();
+            match operator {
+                '+' => Ok(Operation::Addition {
+                    left_addend: Box::new(Operation::Number(left_side)),
+                    right_addend: Box::new(Operation::Number(right_side)),
+                }),
+                '-' => Ok(Operation::Subtraction {
+                    minuend: Box::new(Operation::Number(left_side)),
+                    subtrahend: Box::new(Operation::Number(right_side)),
+                }),
+                '*' => Ok(Operation::Multiplication {
+                    multiplicand: Box::new(Operation::Number(left_side)),
+                    multiplier: Box::new(Operation::Number(right_side)),
+                }),
+                '/' => Ok(Operation::Division {
+                    dividend: Box::new(Operation::Number(left_side)),
+                    divisor: Box::new(Operation::Number(right_side)),
+                }),
+                x => Err(MathStringParseError::InvalidOperatorError {
+                    operator: x,
+                }),
+            }
+        }
+
+        match regex.captures(&input) {
+            None => Err(MathStringParseError::NotParsable),
+            Some(capture) => from_capture(capture)
+        }
     }
 
-    fn execute(&self) -> Result<i32, InvalidOperatorError> {
+    fn execute(&self) -> f64 {
         match self {
-            Operation { left, operator: '+', right } => Ok(left + right),
-            Operation { left, operator: '-', right } => Ok(left - right),
-            Operation { left, operator: '*', right } => Ok(left * right),
-            Operation { left, operator: '/', right } => Ok(left / right),
-            x => Err(InvalidOperatorError {operator: x.operator}),
+            Operation::Number(x) => *x,
+            Operation::Addition {left_addend, right_addend} => left_addend.execute() + right_addend.execute(),
+            Operation::Subtraction {minuend, subtrahend} => minuend.execute() - subtrahend.execute(),
+            Operation::Multiplication {multiplicand, multiplier} => multiplicand.execute() * multiplier.execute(),
+            Operation::Division {dividend, divisor} => dividend.execute() * divisor.execute(),
         }
     }
 }
@@ -43,25 +88,22 @@ fn main() {
 
         if input.trim().eq("quit") {
             println!("exiting...");
-            break
+            break;
         }
 
         let operation = match Operation::from_string(&input) {
-            None => {
+            Err(MathStringParseError::NotParsable) => {
                 println!("Your input was invalid");
-                continue
+                continue;
             },
-            Some(x) => x
+            Err(MathStringParseError::InvalidOperatorError {operator}) => {
+                println!("{} is an invalid operator", operator);
+                continue;
+            }
+            Ok(x) => x,
         };
 
-        let result = match operation.execute() {
-            Err(InvalidOperatorError { operator: op}) => {
-                println!("The operator {} is invalid", op);
-                continue
-            },
-            Ok(x) => x
-        };
-
+        let result = operation.execute();
 
         println!("The result is: {}", result);
     }
